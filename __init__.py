59b9cdf4e6e91eb2bcaa36cbe94881fc3d290ba2
Joshua Boverhof <jrboverhof@lbl.gov>
2004-08-18 23:12:09 +0000

    ----------------------------------------------------------------------
     Modified Files:
     	__init__.py -- cleaned up the module, removed some old code.
    
     ----------------------------------------------------------------------

--- __init__.py
+++ __init__.py
@@ -8,41 +8,14 @@ import XMLname
 from ConfigParser import NoSectionError
 LOGGING = 'logging.txt'
 
+from logging import getLogger
+import logging
+import logging.config
+
 try:
-    from logging import getLogger
-    import logging
-except ImportError, ex:
-    class logger:
-        '''Default logger for python2.2
-        '''
-        def __init__(self, name):
-            self.name = name
-            self.out = out
-
-        def write(self, arg):
-            self.out.write(arg)
-
-        def _write(self, severity, msg, *args, **kw):
-            self.write('%s:  %s  -- %s' %(severity, self.name, msg))
-
-        def error(self, msg, *args, **kw):
-            self._write('ERROR', msg, *args, **kw)
-
-        def warning(self, msg, *args, **kw):
-            self._write('WARNING', msg, *args, **kw)
-
-        def critical(self, msg, *args, **kw):
-            self._write('CRITICAL', msg, *args, **kw)
-
-    def getLogger(name):
-        return logger(name)
-
-else:
-    import logging.config
-    try:
-        logging.config.fileConfig(LOGGING)
-    except (NoSectionError,), ex:
-        logging.basicConfig()
+    logging.config.fileConfig(LOGGING)
+except (NoSectionError,), ex:
+    logging.basicConfig()
 
 class Base:
     def __init__(self, module=__name__):
