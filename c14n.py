dad26034dc7c5baedd0d428199fa66761219b8b7
Joshua Boverhof <jrboverhof@lbl.gov>
2004-12-08 20:04:57 +0000

    ----------------------------------------------------------------------
     Modified Files:
     	c14n.py -- just removed import of ZSI so SOAPy can use
    		w/o ZSI.
     ----------------------------------------------------------------------

--- c14n.py
+++ c14n.py
@@ -5,7 +5,36 @@ No copyright violations -- we're only using parts of PyXML that we
 wrote.
 """
 
-from ZSI import _attrs, _children, _copyright
+_copyright = '''ZSI:  Zolera Soap Infrastructure.
+
+Copyright 2001, Zolera Systems, Inc.  All Rights Reserved.
+Copyright 2002-2003, Rich Salz. All Rights Reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a
+copy of this software and associated documentation files (the "Software"),
+to deal in the Software without restriction, including without limitation
+the rights to use, copy, modify, merge, publish, distribute, and/or
+sell copies of the Software, and to permit persons to whom the Software
+is furnished to do so, provided that the above copyright notice(s) and
+this permission notice appear in all copies of the Software and that
+both the above copyright notice(s) and this permission notice appear in
+supporting documentation.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT
+OF THIRD PARTY RIGHTS. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR HOLDERS
+INCLUDED IN THIS NOTICE BE LIABLE FOR ANY CLAIM, OR ANY SPECIAL INDIRECT
+OR CONSEQUENTIAL DAMAGES, OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS
+OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE
+OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE
+OR PERFORMANCE OF THIS SOFTWARE.
+
+Except as contained in this notice, the name of a copyright holder
+shall not be used in advertising or otherwise to promote the sale, use
+or other dealings in this Software without prior written authorization
+of the copyright holder.
+'''
 
 _copyright += "\n\nPortions are also: "
 _copyright += '''Copyright 2001, Zolera Systems Inc.  All Rights Reserved.
@@ -26,6 +55,8 @@ try:
     from xml.dom.ext import c14n
 except ImportError, ex:
     _implementation2 = None
+    _attrs = lambda E: (E.attributes and E.attributes.values()) or []
+    _children = lambda E: E.childNodes or []
 else:
     class _implementation2(c14n._implementation):
         """Patch for exclusive c14n 
